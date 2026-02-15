be5f2dc7cfb9b624a1d41057e338234c9497f60b
yunie <syafieqayuni@gmail.com>
2026-02-15 17:01:02 +0800

    update Main.java to pass controller

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,24 +1,27 @@
 package app;
 
+import controller.PaymentAndFineController;
+import java.awt.*;
+import javax.swing.*;
 import ui.AdminPanel;
 import ui.EntryExitPanel;
 import ui.ReportingPanel;
-
-import javax.swing.*;
-import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+
+            PaymentAndFineController controller = new PaymentAndFineController();
+
             JFrame frame = new JFrame("Parking Lot Management System");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(1000, 650);
             frame.setLocationRelativeTo(null);
 
             JTabbedPane tabs = new JTabbedPane();
-            tabs.addTab("Admin Panel", new AdminPanel());
-            tabs.addTab("Entry / Exit", new EntryExitPanel());
+            tabs.addTab("Admin Panel", new AdminPanel(controller));
+            tabs.addTab("Entry / Exit", new EntryExitPanel(controller));
             tabs.addTab("Reporting", new ReportingPanel());
 
             frame.add(tabs, BorderLayout.CENTER);
