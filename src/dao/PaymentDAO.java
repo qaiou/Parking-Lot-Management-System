0ae40605db7d071432def7974e90796fcd667c5c
qai <138949452+qaiou@users.noreply.github.com>
2026-02-16 22:25:43 +0800

    delete processpayment

--- src/dao/PaymentDAO.java
+++ src/dao/PaymentDAO.java
@@ -22,20 +22,4 @@ public class PaymentDAO {
         return 0;
     }
 
-    public void processPayment(PaymentAndFine payment) {
-        String sql = "INSERT INTO payments (plate, amount, payment_time) VALUES (?, ?, ?)";
-        try (Connection conn = DBConnect.getConnect();
-            PreparedStatement ps = conn.prepareStatement(sql)) {
-            ps.setString(1, payment.getPlate());
-            ps.setDouble(2, payment.getTotalAmount());
-            ps.setString(3, payment.getPaymentTime().toString()); // or formatted string
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        totalRevenue += payment.getTotalAmount();
-    }
-
-    
 }
